6306c1af69b659fa91cec2dfd725e547652a2003
ich777 <ich777@noreply.example.org>
2020-10-11 07:57:27 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,3 +1,45 @@
 #!/bin/bash
 
+echo "---Resolution check---"
+if [ -z "${CUSTOM_RES_W} ]; then
+	CUSTOM_RES_W=1024
+fi
+if [ -z "${CUSTOM_RES_H} ]; then
+	CUSTOM_RES_H=768
+fi
+
+if [ "${CUSTOM_RES_W}" -le 1023 ]; then
+	echo "---Width to low must be a minimal of 1024 pixels, correcting to 1024...---"
+    CUSTOM_RES_W=1024
+fi
+if [ "${CUSTOM_RES_H}" -le 767 ]; then
+	echo "---Height to low must be a minimal of 768 pixels, correcting to 768...---"
+    CUSTOM_RES_H=768
+fi
+echo "---Checking for old logfiles---"
+find $DATA_DIR -name "XvfbLog.*" -exec rm -f {} \;
+find $DATA_DIR -name "x11vncLog.*" -exec rm -f {} \;
+echo "---Checking for old display lock files---"
+find /tmp -name ".X99*" -exec rm -f {} \; > /dev/null 2>&1
+screen -wipe 2&>/dev/null
+
+chmod -R ${DATA_PERM} ${DATA_DIR}
+
+echo "---Starting Xvfb server---"
+screen -S Xvfb -L -Logfile ${DATA_DIR}/XvfbLog.0 -d -m /opt/scripts/start-Xvfb.sh
+sleep 2
+echo "---Starting x11vnc server---"
+screen -S x11vnc -L -Logfile ${DATA_DIR}/x11vncLog.0 -d -m /opt/scripts/start-x11.sh
+sleep 2
+echo "---Starting Fluxbox---"
+screen -d -m env HOME=/etc /usr/bin/fluxbox
+sleep 2
+echo "---Starting noVNC server---"
+websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/novnc.pem 8080 localhost:5900
+sleep 2
+
 sleep infinity
+
+echo "---Starting Chrome---"
+cd ${DATA_DIR}
+${DATA_DIR}/chromium
